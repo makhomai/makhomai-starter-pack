98ab467a61c9701d311e1e3871102f7fa3b46554
Kevin A <kevin_son@live.nl>
2024-02-02 22:07:15 +0100

    Add Javascript starter bot
    Taken from
    https://github.com/aichallenge/aichallenge/tree/epsilon/ants/dist/starter_bots/javascript

---
+++ starter_bots/javascript/MyBot.js
@@ -0,0 +1,27 @@
+const { ants } = require('./Ants');
+
+const bot = {
+    'onReady': function() {
+		ants.finishTurn();
+    },
+    'onTurn': function() {
+		var myAnts = ants.myAnts();
+		var directions = ['N', 'E', 'S', 'W'];
+		for (var i in myAnts) {
+			var ant = myAnts[i];
+			for (const dirI in directions) {
+				var dir = directions[dirI];
+				var loc = ants.destination(ant.row, ant.col, dir);
+				if (ants.passable(loc[0], loc[1])) {
+					ants.issueOrder(ant.row, ant.col, dir);
+					break;
+				}
+			}
+		}
+		ants.finishTurn();
+    },
+    'onEnd': function() {
+    
+    }
+}
+ants.start(bot);
